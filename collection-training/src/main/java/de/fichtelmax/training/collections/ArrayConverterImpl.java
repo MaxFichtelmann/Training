f712c69333d74e9bc08188f1bb64d54205b8f11b
Max Fichtelmann <max.fichtelmann@gmail.com>
2013-06-17 17:47:13 +0200

    implementation stubs

---
+++ collection-training/src/main/java/de/fichtelmax/training/collections/ArrayConverterImpl.java
@@ -0,0 +1,39 @@
+package de.fichtelmax.training.collections;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ArrayConverterImpl<T> implements ArrayConverter<T>
+{
+    
+    public List<T> convert( T[] array )
+    {
+        // TODO NYI Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+    
+    public Collection<T> unique( T[] input )
+    {
+        // TODO NYI Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+    
+    public Collection<T> unique( Collection<T> input )
+    {
+        // TODO NYI Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+    
+    public Collection<T> combine( T[] first, T[] second )
+    {
+        // TODO NYI Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+    
+    public Collection<T> combine( Collection<T> first, Collection<T> second )
+    {
+        // TODO NYI Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+    
+}
